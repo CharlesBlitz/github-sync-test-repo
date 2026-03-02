36f1a2fbbcb49df42fc83766f3fe78dd626e2e8b
CharlesBlitz <charles@stackblitz.com>
2026-03-02 11:51:55 -0700

    Update project files

---
+++ src/App.jsx
@@ -0,0 +1,13 @@
+import React from 'react';
+import './App.css';
+
+function App() {
+  return (
+    <div className="app">
+      <h1>Hello World</h1>
+      <p>Welcome to your new application!</p>
+    </div>
+  );
+}
+
+export default App;
